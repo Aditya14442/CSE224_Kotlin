7cc72a68b3f1483ebd590b2963914ff70da9b16d
Aditya <aditya14442@gmail.com>
2024-08-21 13:27:10 +0530

    Day 1; Learned var, val and print println

---
+++ day1.kt
@@ -0,0 +1,20 @@
+//The execution of program starts from main() function
+fun main(){
+    //To declare variable use 'var' and 'val' keywords respectively
+    var number = 1
+    val pi = 3.14
+    //Value of variable can be changed while value of constants can not be changed.
+    number=2
+    pi = 2.71 //This line will throw an error
+    //To specify datatype of a variable use the following syntax
+    var name : String = "Jon Doe"
+    //The first letter of the datatype should be capital.
+
+    //print() and println() can be used to print any data to stdout.
+    //println() adds a newline character at the end of line after it is done printing  
+    print("Hello")
+    print("World!")
+    print('\n')
+    println("Hello")
+    println("World!")
+}
